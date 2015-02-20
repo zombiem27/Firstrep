22af75752f04899133111ef4efd2555c7eefead6
zombiem27 <kaitokun16@gmail.com>
2015-02-20 17:40:34 +0700

    versi 0.0.1

---
+++ whatisdis.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>Cuma cumi</title>
+</head>
+<body>
+    <h1 align="center">Aplikasi gak jelas</h1>
+	<form method="post" action="whatisdis_act.php">
+	<table width="452" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#9AC2E3">
+	   <tr>
+			<td height="40" align="center" bgcolor="#9AC2E3"><strong><font color="#FFFFFF">Entry</font></strong></td>
+	   </tr>
+	   <tr>
+			<td bgcolor="#FFFFFF"><table width="452" border="0" align="center" cellpadding="5" cellspacing="0">
+	   </tr>
+	   <tr>
+			<td>Nama</td> <td>:</td>
+			<td><input type="text" name="nama" size="30" maxlength="30"/></td>
+		</tr>
+		<tr>
+			<td>Alamat</td>
+			<td>:</td>
+			<td><textarea name="Alamat" cols="30" rows="5"></textarea></td>
+		</tr>
+		<tr>
+			<td colspan="3" align="center"> <input type="submit" value="OK" name="proses"/> </td>
+		</tr>
+			</table></td>
+	</table>
+	</form>
+</body>
+</html>
---
+++ whatisdis_act.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Hasil Input</title>
+</head>
+<body>
+    <h1>Hasil Input</h1>
+	<?php
+	    $nama = $_REQUEST['nama'];
+		$Alamat =  $_REQUEST['Alamat'];
+		
+		echo "Nama  : $nama<br/>";
+		echo "Alamat : $Alamat<br/>";
+		
+	?>
+</body>
+</html>
